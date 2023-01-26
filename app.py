3c7dc34cd4ce920da12bb2c5a2772943fdebd16c
LuciDevTEC <lucian.kaindl@gmail.com>
2023-01-26 14:01:30 +0100

    update

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
+
+@app.route('/cpu/temp')
+def get_cpu_temp():
+    return '<h1>CPU Temp:</h2>'
+
+@app.route('/disk/usage')
+def get_disk_usage():
+    return '<h1>Disk usage:</h2>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
