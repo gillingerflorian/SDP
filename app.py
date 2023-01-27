b25f8e06157897299802cb54294c44317602ce96
gillingerflorian <gillingerflo@gmail.com>
2023-01-27 12:14:30 +0100

    Added tests, refactored logic

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 from flask import Flask
 app = Flask(__name__)
+import psutil
 
+def get_temp():
+    temp = psutil.sensors_temperatures()["cpu_thermal"][0]
+    return temp
 
+def get_disk_usage():
+    usage = psutil.disk_usage('/').percent
+    return usage
 
-@app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+def get_both():
+    temp = psutil.sensors_temperatures()["cpu_thermal"][0]
+    usage = psutil.disk_usage('/').percent
+    return float(temp.current), str(usage)
+
+@app.route('/error')
+def error():
+    cpu_temp = get_temp()
+    errortemp = 60
+    if float(str(cpu_temp.current)) >= float(errortemp):
+        output = "Critical temperature: " + str(cpu_temp.current)
+    output = "Temperature in good range: " + str(cpu_temp.current)
+    return output
 
 @app.route('/cpu/temp')
 def get_cpu_temp():
-    return '<h1>CPU Temp:</h2>'
+    cpu_temp = get_temp()
+    output = "Temperature: " + str(cpu_temp.current)
+    return output
 
 @app.route('/disk/usage')
-def get_disk_usage():
-    return '<h1>Disk usage:</h2>'
+def get_ram():
+    usage = get_disk_usage()
+    output = "Usage: " + str(usage)
+    return output
 
 
 if __name__ == "__main__":
